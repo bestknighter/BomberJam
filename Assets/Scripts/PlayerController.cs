7028c07f98e82cdf4ac5640d9d919a66ad035e66
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-10-21 01:12:17 -0200

    Feature/input (#4)
    * Adiciona versao 1 do input controller
    
    * PlayerController mais robusto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,15 @@ public class PlayerController : MonoBehaviour {
 	private int PlayerNumber = 1;
 
 	public MoveDirection DesiredDirection {
-		get { return m_Act1; }
+		get { return m_Dir1; }
+	}
+
+	public MoveDirection OptionalDirection {
+		get { return m_Dir2; }
+	}
+
+	public MoveDirection ChosenDirection {
+		set { m_ChosenDir = value; }
 	}
 
 	public bool BotaoA {
@@ -17,16 +25,16 @@ public class PlayerController : MonoBehaviour {
 		get { return m_BtnB; }
 	}
 
-	private MoveDirection m_Act1;
-	private MoveDirection m_Act2;
-	private MoveDirection m_ChosenAct;
+	private MoveDirection m_Dir1;
+	private MoveDirection m_Dir2;
+	private MoveDirection m_ChosenDir;
 	private bool m_BtnA;
 	private bool m_BtnB;
 
 	void Start () {
-		m_Act1 = MoveDirection.NOTHING;
-		m_Act2 = MoveDirection.NOTHING;
-		m_ChosenAct = MoveDirection.NOTHING;
+		m_Dir1 = MoveDirection.NOTHING;
+		m_Dir2 = MoveDirection.NOTHING;
+		m_ChosenDir = MoveDirection.NOTHING;
 		m_BtnA = false;
 		m_BtnB = false;
 	}
@@ -41,35 +49,37 @@ public class PlayerController : MonoBehaviour {
 		float v = Input.GetAxis ("VerticalP" + PlayerNumber);
 
 		if (h > 0.0f) {
-			m_Act1 = MoveDirection.RIGHT;
+			opt1 = MoveDirection.RIGHT;
 		} else if (h < 0.0f) {
-			m_Act1 = MoveDirection.LEFT;
-		} else if (v > 0.0f) {
-			m_Act1 = MoveDirection.UP;
-		} else if (v < 0.0f) {
-			m_Act1 = MoveDirection.DOWN;
+			opt1 = MoveDirection.LEFT;
 		} else {
-			m_Act1 = MoveDirection.NOTHING;
+			opt1 = MoveDirection.NOTHING;
 		}
 
-//		if (v > 0.0f) {
-//			opt2 = MoveDirection.UP;
-//		} else if (v < 0.0f) {
-//			opt2 = MoveDirection.DOWN;
-//		} else {
-//			opt2 = MoveDirection.NOTHING;
-//		}
+		if (v > 0.0f) {
+			opt2 = MoveDirection.UP;
+		} else if (v < 0.0f) {
+			opt2 = MoveDirection.DOWN;
+		} else {
+			opt2 = MoveDirection.NOTHING;
+		}
 
-//		if (opt1 == m_ChosenAct) {
-//			m_Act1 = opt1;
-//			m_Act2 = opt2;
-//		} else if (opt2 == m_ChosenAct) {
-//			m_Act1 = opt2;
-//			m_Act2 = opt1;
-//		}
+		if (MoveDirection.NOTHING == opt1) {
+			m_Dir1 = opt2;
+			m_Dir2 = opt1;
+		} else if (MoveDirection.NOTHING == opt2) {
+			m_Dir1 = opt1;
+			m_Dir2 = opt2;
+		} else if (m_ChosenDir == opt1) {
+			m_Dir1 = opt1;
+			m_Dir2 = opt2;
+		} else {
+			m_Dir1 = opt2;
+			m_Dir2 = opt1;
+		}
 	}
 
 	void Update () {
-//		Debug.Log ("ChosenInput: " + m_Act1 + "\nA: " + m_BtnA + "\tB: " + m_BtnB);
+//		Debug.Log ("Direction:\tDesired: " + m_Dir1 + "\tOptional: " + m_Dir2 + "\tChosen: " + m_ChosenDir + "\nA: " + m_BtnA + "\tB: " + m_BtnB);
 	}
 }
