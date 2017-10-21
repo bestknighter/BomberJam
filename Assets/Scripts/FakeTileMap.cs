0d25112df0e2ce3eb79bd9312920f4a3f45f15f9
Iago Lobo <iago.wolf.moraes@gmail.com>
2017-10-21 03:54:43 -0200

    Cria flippers randomizados por array e sem ser um em cima do outro

--- Assets/Scripts/FakeTileMap.cs
+++ Assets/Scripts/FakeTileMap.cs
@@ -6,6 +6,7 @@ public class FakeTileMap : MonoBehaviour {
 
 	public GameObject obstaculo;
 	public int numObstaculos;
+	public int numMaxObstaculos;
 
 	public int x_pixels;
 	public int y_pixels;
@@ -18,6 +19,9 @@ public class FakeTileMap : MonoBehaviour {
 	[SerializeField]
 	private int square_height;
 
+	private int[] indexes;
+
+	/*
 	Vector2 SortSquare()
 	{
 		int sortedValor = Random.Range (0, num_horizontalsquares * num_verticalsquares);
@@ -33,22 +37,37 @@ public class FakeTileMap : MonoBehaviour {
 		int x = Random.Range(1, num_verticalsquares-1);
 		int y = Random.Range (1, num_horizontalsquares - 1);
 
-		Vector2 ret = new Vector2 (x * square_width+xOffset, y * square_height);
+		Vector2 ret = new Vector2 (x * square_width+xOffset + square_width/2, y * square_height + square_height/2);
 
 		return ret;
-	}
+	}*/
 
 	// Use this for initialization
-	void Start () { 
-		for (int i = 0; i < numObstaculos; i++) 
-		{
-			Vector2 spawnPos = SortSquare ();
+	void Start () {
+		if (numObstaculos > numMaxObstaculos) {
+			Debug.LogError ("Proibido numObstaculos ser maior que numMaxObstaculos");
+		}
+		indexes = new int[numMaxObstaculos];
+		for (int i = 0; i < numMaxObstaculos; i++) {
+			indexes [i] = i;
+		}
+
+		for (int i = 0; i < numMaxObstaculos; i++) {
+			int tmp = indexes [i];
+			int j = Random.Range (i, numMaxObstaculos);
+			indexes [i] = indexes [j];
+			indexes [j] = tmp;
+		}
+
+		for (int i = 0; i < numObstaculos; i++) {
+			int x = indexes[i] % (num_verticalsquares-2)+1;
+			int y = indexes[i] / (num_verticalsquares-2)+1;
+			Vector2 spawnPos = new Vector2 (x * square_width+xOffset + square_width/2, y * square_height + square_height/2);
 			Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(spawnPos.x, spawnPos.y, 0f));
 			worldPos.z = -2;
 			GameObject.Instantiate (obstaculo, worldPos, Quaternion.identity);
 		}
-
-
+			
 	}
 	
 	// Update is called once per frame
